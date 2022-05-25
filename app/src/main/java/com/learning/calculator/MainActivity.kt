b9b5b74430f90767b7b95b386f26719a5a15b44f
yussefoh <youssef.akil@sainsburys.co.uk>
2022-05-25 11:24:15 +0100

    CalculatorApp [feature]: Added onDigit functionality to allow digits that are pressed to be displayed and onClear functionality which allows the CLR button to clear the digits on screen

--- app/src/main/java/com/learning/calculator/MainActivity.kt
+++ app/src/main/java/com/learning/calculator/MainActivity.kt
@@ -3,15 +3,25 @@ package com.learning.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit (view:View) {
-        Toast.makeText(this, "ButtonClick", Toast.LENGTH_LONG).show()
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
